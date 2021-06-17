45a46e73bfe1856c1d1552edde95fc0b98895cf9
whitehatjr-github <73145652+whitehatjr-github@users.noreply.github.com>
2021-06-16 23:13:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+//the red box
+var bottomBox, rightBox, leftBox;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	//making the boxes non-static
+	//Matter.Body.setStatic(bottomBox, false);
+	//Matter.Body.setStatic(rightBox, false);
+	//Matter.Body.setStatic(leftBox, false);
+
+	//creating the boxes
+	bottomBoxSprite=createSprite(400,650,200,20);
+	rightBoxSprite=createSprite(300,610,20,100);
+	leftBoxSprite=createSprite(500,610,20,100);
+
+	//making the red color for the boxes
+	bottomBoxSprite.shapeColor=color(255,0,0);
+	rightBoxSprite.shapeColor=color(255,0,0)
+	leftBoxSprite.shapeColor=color(255,0,0);
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	Matter.Body.setStatic(packageBody, false);
+    
+  }
+}
+
+
+
